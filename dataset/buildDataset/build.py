ca8ac4f51fd1146431b86aa78066afe10ae29cf3
Lukas Ottenhof <ottenlury@gmail.com>
2026-09-02 11:14:28 -0600

    ready to redo agent

--- dataset/buildDataset/build.py
+++ dataset/buildDataset/build.py
@@ -1157,12 +1157,11 @@ if __name__ == "__main__":
     # WE MINE AGENT PRS, THEN WE MINE DEV PRS. DEV PRS ARE SAVED IN "dev_final_dataset_subset_b.csv"
     # WHILE AGENT GO TO "agent_final_dataset_subset_b.csv"
 
-    # output_path = OUTPUT_DIR / "aidev_final_dataset.csv"
-    # stats_path = OUTPUT_DIR / "mining_stats_human.json"
-    output_path = OUTPUT_DIR / "dev_dataset_subset_data.csv"
-    stats_path = OUTPUT_DIR / "stats_dev_subset_data.json"
-    # output_path = OUTPUT_DIR / "agent_dataset_subset_data.csv"
-    # stats_path = OUTPUT_DIR / "stats_agent_subset_data.json"
+
+    # output_path = OUTPUT_DIR / "dev_dataset_subset_data.csv"
+    # stats_path = OUTPUT_DIR / "stats_dev_subset_data.json"
+    output_path = OUTPUT_DIR / "agent_dataset_subset_data.csv"
+    stats_path = OUTPUT_DIR / "stats_agent_subset_data.json"
     if stats_path.exists():
         with open(stats_path, 'r') as sj:
             final_stats = Counter(json.load(sj))
